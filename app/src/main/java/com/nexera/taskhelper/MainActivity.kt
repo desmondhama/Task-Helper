54ebc1778431f37d4ea8d819bde663f4a8453c0b
desmondhama <desmondhama@gmail.com>
2025-03-07 18:55:23 +0200

    Updated MainActivity, strings.xml, and drawable XML files

--- app/src/main/java/com/nexera/taskhelper/MainActivity.kt
+++ app/src/main/java/com/nexera/taskhelper/MainActivity.kt
@@ -3,27 +3,64 @@ package com.nexera.taskhelper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nexera.taskhelper.ui.theme.MyComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ImageBanner() // Use ImageBanner instead of Greeting
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ImageBanner(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null // Add contentDescription
+        )
+        Text(
+            text = stringResource(id = R.string.task_result),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp) // Fix padding
+        )
+		 Text(
+            text = stringResource(id = R.string.nice_work),
+            style = TextStyle(
+                fontSize = 16.sp
+            )
+		)	
     }
 }
